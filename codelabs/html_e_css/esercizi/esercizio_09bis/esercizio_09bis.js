f76e46662aad1381d94ecaa3d25b34fb548e6bb2
Massimo Zancanaro <massimo.zancanaro@gmail.com>
2024-04-28 22:23:38 +0200

    .

---
+++ codelabs/html_e_css/esercizi/esercizio_09bis/esercizio_09bis.js
@@ -0,0 +1,52 @@
+let espressione="";
+
+window.addEventListener("load", (event) => {
+
+    for (let i=0; i<10;i++ ) {
+        let numElement=document.getElementById("n"+i);
+        numElement.addEventListener("click", (event) => {
+            console.log("i="+i)
+            if ( espressione==="ERROR") {
+                espressione="";
+            }
+            espressione+=i.toString();
+            document.getElementById("risultato").textContent=espressione;
+        });
+    }
+
+    for (let element of document.getElementsByClassName("operazione")) {
+        console.log(element)
+        element.addEventListener("click",(event) => {
+            switch (event.target.id) {
+                case "operazioneSomma":
+                    espressione+="+";
+                    break;
+                case "operazioneSottrazione":
+                    espressione+="-";
+                    break;
+                case "operazioneProdotto":
+                    espressione+="*";
+                    break;
+                case "operazioneDivisione":
+                    espressione+="/";
+                    break;
+                default:
+            }
+            document.getElementById("risultato").textContent=espressione;
+        })
+    }
+
+    document.getElementById("uguale").addEventListener("click", (event) => {
+        try {
+           espressione=eval(espressione);
+        } catch (error) {
+            espressione="ERROR";
+        }
+        document.getElementById("risultato").textContent=espressione;
+    });
+    document.getElementById("cancellazione").addEventListener("click", (event) => {
+        espressione="";
+        document.getElementById("risultato").textContent=espressione;
+    });
+    
+});
