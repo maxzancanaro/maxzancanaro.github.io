f2558283350b31dd3377c2bac7ca32a15bd18092
Massimo Zancanaro <massimo.zancanaro@gmail.com>
2024-04-28 10:38:34 +0200

    .

---
+++ codelabs/html_e_css/tw/tw_test_breakpoints.js
@@ -0,0 +1,12 @@
+
+function visualizzaRisoluzione () {
+    elementWidth = document.getElementById("window_width")
+    elementHeight = document.getElementById("window_height")
+
+    elementWidth.textContent = window.innerWidth;
+    elementHeight.textContent = window.innerHeight;
+}
+
+window.addEventListener("resize", visualizzaRisoluzione);
+
+window.addEventListener("load", visualizzaRisoluzione);
