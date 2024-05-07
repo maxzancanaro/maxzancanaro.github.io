41ee18787d70ebeed3e22dbc1189fb329a153603
Massimo Zancanaro <massimo.zancanaro@gmail.com>
2024-05-07 23:47:28 +0200

    .

---
+++ codelabs/html_e_css/esercizi/esercizio_09bis_minimo/esercizio_09bis.js
@@ -0,0 +1,45 @@
+let espressione="";
+
+window.addEventListener("load", (event) => {
+
+    let elementoNumeri=document.getElementById("numeri")
+    for (let element of elementoNumeri.children) {
+        
+        element.addEventListener("click", (event) => {
+            if ( espressione==="ERROR") {
+                espressione="";
+            }
+            espressione+=event.target.textContent;
+            document.getElementById("risultato").textContent=espressione;
+        });
+    }
+
+    let elementoOperazioni=document.getElementById("operazioni")
+    for (let element of elementoOperazioni.children) {
+ 
+       element.addEventListener("click",(event) => {
+            if ( event.target.id === "cancella" ) {
+                espressione=""; 
+            } else {
+                espressione+=event.target.textContent;
+            }
+            document.getElementById("risultato").textContent=espressione;
+        })
+    }
+
+    document.getElementById("uguale").addEventListener("click", (event) => {
+        try {
+           espressione=math.evaluate(espressione);
+           espressione=parseFloat(espressione.toFixed(11)).toString();
+           //espressione=eval(espressione);
+        } catch (error) {
+            espressione="ERROR";
+        }
+        document.getElementById("risultato").textContent= espressione;
+    });
+    document.getElementById("cancellazione").addEventListener("click", (event) => {
+        espressione="";
+        document.getElementById("risultato").textContent=espressione;
+    });
+    
+});
